cfde868b6c66bdc6b41b03458870394a11b83abe
adnanqrsh01 <135521491+adnanqrsh01@users.noreply.github.com>
2023-06-14 09:14:22 +0530

    Update __init__.py

--- luffy/__init__.py
+++ luffy/__init__.py
@@ -7,6 +7,6 @@ openai_key = "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"
 
 api_id = "1125689"
 api_hash = "4772d1792ed194020a8fb06a91ffb8fa"
-bot_token="6243206260:AAHvqSvlOUWcycCiL5eRE0RSwyBkMBodTKs"
+bot_token="6243206260:AAHLYG28m8GL5Gs4CfXbaDyoFk9VLJXyHF4"
 
 bot=TelegramClient("Robinbig",api_id,api_hash).start(bot_token = bot_token)
