0813564a905adce6c4fd8a0632784bdc607a45f2
adnanqrsh01 <135521491+adnanqrsh01@users.noreply.github.com>
2023-06-14 09:45:04 +0530

    Update Ytdl.py

--- luffy/plugins/Ytdl.py
+++ luffy/plugins/Ytdl.py
@@ -182,15 +182,15 @@ def humanbytes(size):
     outputs in a human readable format"""
     # https://stackoverflow.com/a/4936172/4723940
 if not size:
-        return ""
+    return ""
     # 2 ** 10 = 1024
-    power = 2 ** 10
-    raised_to_pow = 0
-    dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
-    while size > power:
-        size /= power
-        raised_to_pow += 1
-    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
+power = 2 ** 10
+raised_to_pow = 0
+dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
+while size > power:
+    size /= power
+    raised_to_pow += 1
+return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
 
 
 def time_formatter(milliseconds: int) -> str:
